f5a29945ac5710c4f88c68e7aa75d64a84743d23
juliano acuña <Juliano.acunia@radiumrocket.com>
2020-11-10 20:26:10 -0300

    Function addHighScore

--- js/app.js
+++ js/app.js
@@ -120,6 +120,18 @@
             return ~~(Math.random() * max);
         }
 
+        function addHighscore(score) {
+            posHighscore = 0;
+            while (highscores[posHighscore] > score && posHighscore < highscores.length) {
+                posHighscore += 1;
+            }
+            highscores.splice(posHighscore, 0, score);
+            if (highscores.length > 10) {
+                highscores.length = 10;
+            }
+            localStorage.highscores = highscores.join(',');
+        }
+
 
 
 })
