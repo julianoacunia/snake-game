2cb29f57c8d01e864e2e1ca852b679cae67c39d5
juliano acuña <Juliano.acunia@radiumrocket.com>
2020-11-10 20:22:45 -0300

    Function Scene

--- js/app.js
+++ js/app.js
@@ -56,6 +56,61 @@
                 };
         }());
 
-        
+        document.addEventListener('keydown', function (evt) {
+            if (evt.which >= 37 && evt.which <= 40) {
+                evt.preventDefault();
+            }
+            lastPress = evt.which;
+        }, false);
+        function Rectangle(x, y, width, height) {
+            this.x = (x === undefined) ? 0 : x;
+            this.y = (y === undefined) ? 0 : y;
+            this.width = (width === undefined) ? 0 : width;
+            this.height = (height === undefined) ? this.width : height;
+        }
+        Rectangle.prototype = {
+            constructor: Rectangle,
+            intersects: function (rect) {
+                if (rect === undefined) {
+                    window.console.warn('Missing parameters on function intersects');
+                } else {
+                    return (this.x < rect.x + rect.width &&
+                        this.x + this.width > rect.x &&
+                        this.y < rect.y + rect.height &&
+                        this.y + this.height > rect.y);
+                }
+            },
+            fill: function (ctx) {
+                if (ctx === undefined) {
+                    window.console.warn('Missing parameters on function fill');
+                } else {
+                    ctx.fillRect(this.x, this.y, this.width, this.height);
+                }
+            },
+            drawImage: function (ctx, img) {
+                if (img === undefined) {
+                    window.console.warn('Missing parameters on function drawImage');
+                } else {
+                    if (img.width) {
+                        ctx.drawImage(img, this.x, this.y);
+                    } else {
+                        ctx.strokeRect(this.x, this.y, this.width, this.height);
+                    }
+                }
+            }
+        };
+
+        function Scene() {
+            this.id = scenes.length;
+            scenes.push(this);
+        };
+        Scene.prototype = {
+            constructor: Scene,
+            load: function () { },
+            paint: function (ctx) { },
+            act: function () { }
+        };
+
+
 
 })
